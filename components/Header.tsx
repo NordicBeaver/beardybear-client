bf3504f8ca5c2f10f4316157d71e2aa37eac5c1f
NordicBeaver <nordicbeaver@gmail.com>
2021-09-29 09:48:00 +0300

    Header styles.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <span>Beardy Bear</span>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/barbers">
-            <a>Barbers</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/services">
-            <a>Services</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/bookAppointment">
-            <a>Book</a>
-          </Link>
-        </li>
-      </ul>
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <span className={styles.logo}>Beardy Bear</span>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link href="/barbers">
+                <a>Barbers</a>
+              </Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link href="/services">
+                <a>Services</a>
+              </Link>
+            </li>
+            <li className={styles.menuItem}>
+              <Link href="/bookAppointment">
+                <a>Book</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 }
