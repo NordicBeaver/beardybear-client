debc48bd8443a3466509c9a3b7ece8dfc7d90ede
NordicBeaver <nordicbeaver@gmail.com>
2021-09-29 14:04:58 +0300

    Change font to Poppins.

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&amp;display=swap"
+          rel="stylesheet"
+        ></link>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
