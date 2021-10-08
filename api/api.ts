8c012cf0eb87cc762758a853233319075e362fee
NordicBeaver <nordicbeaver@gmail.com>
2021-10-08 11:19:13 +0300

    Fetch barbers on Barbers page.

--- api/api.ts
+++ api/api.ts
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const host = 'http://localhost:3000';
 
+export interface BarberDto {
+  id: number;
+  name: string;
+  description: string;
+  picture: string | null;
+}
+
+export async function getBarbers() {
+  const response = await axios.get<BarberDto[]>(`${host}/barbers`);
+  const data = response.data;
+  return data;
+}
+
 export interface BarberServiceDto {
   id: number;
   name: string;
--- pages/barbers.tsx
+++ pages/barbers.tsx
@@ -1,19 +1,41 @@
+import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
+import { BarberDto, getBarbers } from '../api/api';
 import Container from '../components/Container';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import styles from './barbers.module.css';
 
-export default function Barbers() {
+interface BarbersPageProps {
+  barbers: BarberDto[];
+}
+
+const BarbersPage: NextPage<BarbersPageProps> = ({ barbers }) => {
   return (
     <div className={styles.page}>
       <Header></Header>
 
       <Container>
         <h1>Barbers</h1>
+        <ul>
+          {barbers.map((barber) => (
+            <li key={barber.id}>{barber.name}</li>
+          ))}
+        </ul>
       </Container>
 
       <Footer></Footer>
     </div>
   );
-}
+};
+
+export default BarbersPage;
+
+export const getServerSideProps: GetServerSideProps<BarbersPageProps> = async (constext) => {
+  const barbers = await getBarbers();
+  return {
+    props: {
+      barbers: barbers,
+    },
+  };
+};
