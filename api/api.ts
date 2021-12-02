af0002137e83a781eb5cb5d1f276ee4356b97f0d
NordicBeaver <nordicbeaver@gmail.com>
2021-12-02 13:15:02 +0300

    Fix api url env variable name.

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 
-const host = process.env.NEXT_PUBLIC_ANALYTICS_ID ?? 'http://localhost:3000';
+const host = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3000';
 
 export function imageUrl(filename: string) {
   return `${host}/images/${filename}`;
